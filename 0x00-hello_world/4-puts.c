f630e29dca415d5ac25bdc32755ee3265cc3f013
Phumzile Moloto <pzmoloto01@gmail.com>
2023-02-17 02:48:26 -0800

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+/** main - A program that prints a line with puts finction
+ *Return:0 (Success) 
+ */
+int main(void)
+{ 
+puts("\"Programming is like building a miltilingual puzzle"); 
+return (0);
+} 
