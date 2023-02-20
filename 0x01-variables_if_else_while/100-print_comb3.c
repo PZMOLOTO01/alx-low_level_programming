f91b117a767522f21f5738629344721f5fa935ac
Phumzile Moloto <pzmoloto01@gmail.com>
2023-02-20 12:30:42 -0800

    combo3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int c, i;
 
-	for (c = '0'; c < = '9'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		for (i = '0'; i <= '9'; i++)
 		{
@@ -18,9 +18,9 @@ int main(void)
 				putchar(c);
 				putchar(i);
 
-				if (c != '8'; || (c == '8' &&  i != '9'))
+				if (c != '8' || (c == '8' &&  i != '9'))
 				{
-					putchar(' , ');
+					putchar(',');
 					putchar(' ');
 				}
 			}
