0d38e9efeb207a843f166572f82b6e4f30ecf2bd
Phumzile Moloto <pzmoloto01@gmail.com>
2023-02-21 04:51:57 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,12 +3,12 @@
  * print aplhabet - print lowercase alphabets followed by new line
  * Return: 0
  */
-void print_alphabet(void)
+void print_alphabet(void);
 {
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
 	_putchar('\n');
-	return (0);
 }
+
