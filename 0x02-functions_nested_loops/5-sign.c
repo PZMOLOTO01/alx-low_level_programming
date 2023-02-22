a22dea80ac9cacb8882facf2ce9aa90309df454a
Phumzile Moloto <pzmoloto01@gmail.com>
2023-02-22 10:59:47 -0800

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,7 +8,7 @@
  * -1 and prints - if n is less than zero
  */
 
-int print_sign(int n);
+int print_sign(int n)
 {
 	if (n > 0)
 	{
@@ -16,4 +16,14 @@ int print_sign(int n);
 		return (1);
 	}
 	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('+');
+		return (-1);
+	}
 }
+
