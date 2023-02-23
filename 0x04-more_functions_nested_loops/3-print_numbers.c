1bd6c8e8cb272079ee531a94d6676f885a23d5bb
Phumzile Moloto <pzmoloto01@gmail.com>
2023-02-23 09:04:23 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - checks for a digit 0 through 9
+ * Return: always 0
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		_putchar(c + '0');
+	}
+	_putchar('\n');
+}
