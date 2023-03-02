a5c01a77d6d4e0f4e17a279d434218198c9d373e
Phumzile Moloto <pzmoloto01@gmail.com>
2023-03-02 10:22:38 -0800

    toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * string_toupper -change all lowercase to uppercase
- * @str: string to be changed
- * Return: a pointer string to be changed
+ * string_toupper - Change all lowercase to uppercase
+ * @str: pointer
+ * Return: n
  */
 char *string_toupper(char *str)
 {
