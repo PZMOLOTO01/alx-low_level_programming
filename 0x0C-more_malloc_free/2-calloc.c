8d7121d53b99538661904565fa5a407d6724728a
Phumzile Moloto <pzmoloto01@gmail.com>
2023-03-17 13:26:30 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -18,7 +18,7 @@ char *_memset(char *s, char b, unsigned int n)
 	return (s);
 }
 /**
- * _calloc - allocates memory for an array
+ * *_calloc - allocates memory for an array
  * @nmemb: number of elements in the array
  * @size: size of each element
  * Return: pointer to allocated memory
